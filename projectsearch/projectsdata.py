38675cb601235d62738b47144cdb5194921b18d9
iniyaa <iniyaam21@gmail.com>
2021-06-15 21:56:50 -0700

    added scrum team project data

--- projectsearch/projectsdata.py
+++ projectsearch/projectsdata.py
@@ -24,11 +24,11 @@ def PieceofthePI():
     scrum_team = "wave2"
     trimester = 3
     keyword = "statistics"
-    gitlink = "#"
-    comlink = "#"
+    gitlink = "https://github.com/BradleyBartelt/P2__Waves"
+    comlink = "https://youtu.be/hc_ZCVRqu34"
     runlink = "http://pieceofthepi.pusdcoders.tk/"
-    scrumlink = "#"
-    readmelink = "#"
+    scrumlink = "https://github.com/BradleyBartelt/P2__Waves"
+    readmelink = "https://github.com/BradleyBartelt/P2__Waves"
     description = "Our project is a pizza social media. To get the best slice of pizza. On our site you can leave reviews on pizza, chat with other users on pizza related topics, upload pictures of pizza, and navigate external pizza sites, all in one location."
     return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
 
@@ -40,11 +40,11 @@ def GamesFromtheDecades():
     scrum_team = "snakeyees"
     trimester = 3
     keyword = "statistics"
-    gitlink = "#"
+    gitlink = "https://github.com/keviin0/CSA-Portfolio-GroceryStoreSim"
     comlink = "#"
-    runlink = "#"
-    scrumlink = "#"
-    readmelink = "#"
+    runlink = ""
+    scrumlink = "https://github.com/keviin0/CSA-Portfolio-GroceryStoreSim"
+    readmelink = "https://github.com/keviin0/CSA-Portfolio-GroceryStoreSim"
     description = "This project allowed the user to play different video games from different decades. The games specifically were Minesweeper, Uno, and Go Fish."
     return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
 
@@ -52,16 +52,353 @@ def GamesFromtheDecades():
 def Covid19TeaShop():
     image = ""
     title = "Covid-19 Tea Shop"
-    members = "Siddhant Ranka, Aidan Rosen, Andrew Hale, "
+    members = "Siddhant Ranka, Aidan Rosen, Andrew Hale"
     scrum_team = "Coconuts"
     trimester = 2
     keyword = "Commerce adn Data"
     gitlink = "https://github.com/Siddhant8/p1-coconuts2"
     comlink = "https://drive.google.com/file/d/1stjN2g0_sbFWiXRue-2ZFMkNQsxnuwyK/view"
     runlink = "#"
-    scrumlink = "#"
+    scrumlink = "https://github.com/Siddhant8/p1-coconuts2"
     readmelink = "https://github.com/Siddhant8/p1-coconuts2"
     description = "An online website was created to simulate an online tea shop. A coronavirus calculator was created to calculate cases during the covid pandemic, and a jukebox was created to make shopping more enjoyable."
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+def OnlineGroceryStore():
+    image = ""
+    title = "Online Grocery Store"
+    members = "Siddhant Ranka, Sean Tran, Aditya Surapaneni, Jaco Rozenkrants, Kevin Hu"
+    scrum_team = "Grocerytore"
+    trimester = 1
+    keyword = "Commerce/Trade"
+    gitlink = "https://replit.com/@KevinHu10/Grocery-Store#Main.java"
+    comlink = ""
+    runlink = "https://replit.com/@KevinHu10/Grocery-Store#Main.java"
+    scrumlink = "https://replit.com/@KevinHu10/Grocery-Store#Main.java"
+    readmelink = "https://replit.com/@KevinHu10/Grocery-Store#Main.java"
+    description = "This project simulated an online grocery store."
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def MusicWebsite():
+    image = ""
+    title = "Music Website"
+    members = "Eshaan Parlikar, Chris Rubin, Komay Sugiyama, Charlie Zhu, Devam S"
+    scrum_team = "kpop"
+    trimester = 3
+    keyword = "Music"
+    gitlink = "https://github.com/zenxha/kpop"
+    comlink = ""
+    runlink = "#"
+    scrumlink = "https://github.com/zenxha/kpop"
+    readmelink = "https://github.com/zenxha/kpop"
+    description = "The purpose of our project is to give users access to new playlists and discover music for their taste. A cool feature our website holds is that it includes a random playlist generator on the homepage."
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def TheGatorsCalculatorHub():
+    image = ""
+    title = "The Gators Calculator Hub"
+    members = "Aryan Amberkar, Pranav Kambhampati, Praveen Natarajan, Neil Sahai"
+    scrum_team = "Gators"
+    trimester = 2
+    keyword = "School/Education"
+    gitlink = "https://github.com/PranavKambhampati/CalculatorHubPlayground"
+    comlink = "https://github.com/PranavKambhampati/CalculatorHubPlayground "
+    runlink = ""
+    scrumlink = "https://github.com/PranavKambhampati/CalculatorHubPlayground"
+    readmelink = "https://github.com/PranavKambhampati/CalculatorHubPlayground"
+    description = "We created a calculator hub where the user can perform simple calculations powered by JavaScript, CSS, HTML, Java Spring and Thymeleaf. Users are able to use a trigonometric calculator, conversions calculator, logarithmic calculator, and a simple 4 function calculator. "
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def SurveyDrone():
+    image = ""
+    title = "Survey Drone"
+    members = "Pranav Kambhampati, Nakul Nandhakumar, Aryan Amberkar, Michael Hayes"
+    scrum_team = "Drone"
+    trimester = 3
+    keyword = "School/Education, Statistics/Information/Data"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def HexPass():
+    image = ""
+    title = "HexPass"
+    members = "Alex Hu, Aidan Rosen, Andrew Hale, Andrew Joseph, Atharva Kudrimoti"
+    scrum_team = "P1A"
+    trimester = 1
+    keyword = "School/Education, Statistics/Information/Data, Technology"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+
+def COVID19Tracker():
+    image = ""
+    title = "Covid-19 Tracker"
+    members = "Pragdeesh Raj, Ayman Kazi, Yazhisai Rajaraman, Karam Alshaikh"
+    scrum_team = "Platypodes"
+    trimester = 2
+    keyword = "Statistics/Information/Data"
+    gitlink = ""
+    comlink = "https://www.youtube.com/watch?v=9CB3u6HePH8"
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+
+def Hangman():
+    image = ""
+    title = "Hangman"
+    members = "Noah Pidding, Sarah Xie, Charlie Zhu, Rivan Nayak, Rohan Nallapati"
+    scrum_team = "Hangman"
+    trimester = 1
+    keyword = "Games/Gaming"
+    gitlink = ""
+    comlink = "https://drive.google.com/file/d/1O2V0B20Kp5_JQtiEtKFJAB253Y8NNJsM/view?usp=sharing"
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def CypherCrypto():
+    image = ""
+    title = "CypherCrypto"
+    members = "Noah Pidding, Nihar Marar, Rivan Nayak, Adam Holbel"
+    scrum_team = "Wildcats"
+    trimester = 2
+    keyword = "Technology"
+    gitlink = ""
+    comlink = "https://www.youtube.com/watch?v=M6lY3TwUoTY"
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def CowboyTown():
+    image = ""
+    title = "Cowboy Town"
+    members = "Noah Pidding, Aiden Tung, Nathaniel Lee, Dane Vestal"
+    scrum_team = "Cowboys"
+    trimester = 3
+    keyword = "Games/Gaming, Museums"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def CowboysAdvertisement():
+    image = ""
+    title = "Cowboys Advertisement"
+    members = "William Cherres, Allen Xu, Marc Humeau, Karam Alshaikh, Tanmay Marwah"
+    scrum_team = "Cowboys"
+    trimester = 3
+    keyword = "Social Media"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def PokemonGame():
+    image = ""
+    title = "Pokemon Game"
+    members = "Abhijay Deevi, Aiden Cizek, Akshit Prathipati, Megan Corrigan, Zachary Joseph"
+    scrum_team = "Slackbots"
+    trimester = 3
+    keyword = "Games/Gaming"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def TheSmack():
+    image = ""
+    title = "The Smack"
+    members = "Ava Brooks, Eva Gravin, Risa Iwazaki, Linda Long, "
+    scrum_team = "Kangaroos"
+    trimester = 3
+    keyword = "Social Media"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def JavaSwingTeaShop():
+    image = ""
+    title = "Java Swing Tea Shop"
+    members = "Siddhant Ranka, Andrew Hale, Aidan Rosen"
+    scrum_team = "Coconuts"
+    trimester = 2
+    keyword = "Commerce/Trade"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def Recipebox():
+    image = ""
+    title = "Recipebox"
+    members = "Sophie Lee, Eva Gravin, Linda Long, Ali Saad"
+    scrum_team = "Slugs"
+    trimester = 2
+    keyword = "Games/Gaming, Food"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def NighthawkPlanner():
+    image = ""
+    title = "Nighthawk Planner"
+    members = "Mackenzie Aboy, Pedro de Medeiros, Anthony Giustiniano, Naweid Hassanzadeh, Cherry Ding"
+    scrum_team = "Anteaters"
+    trimester = 3
+    keyword = "School/Education, Organization"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def TigersShopWebsite():
+    image = ""
+    title = "Tigers Shop Website"
+    members = "Rohan Nallapati, Tanmay Marwah, Charlie Zhu, Eshaan Parlikar"
+    scrum_team = "Tigers"
+    trimester = 2
+    keyword = "Commerce/Trade"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def Uno():
+    image = ""
+    title = "Uno!"
+    members = "Alex Hu, Atharva Kudrimoti, Andrew Pu, Kevin Hu, Sean Tran"
+    scrum_team = "More Rice"
+    trimester = 3
+    keyword = "Games/Gaming"
+    gitlink = ""
+    comlink = "https://drive.google.com/file/d/12i--RR0FyqHeI86T77Li56oPTxmSA7lh/view"
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def ImagePostingDiscussionJavaMiniLabs():
+    image = ""
+    title = "Image Posting/Discussion Java Mini Labs"
+    members = "Dominic Phung, Sean Rollins, Jason Chang, Andrew Joseph"
+    scrum_team = "Rain"
+    trimester = 3
+    keyword = "Social Media"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def BoomerESports():
+    image = ""
+    title = "Boomer ESports"
+    members = "Aditya Surapaneni, Siddhant Ranka, Jacob Rozenkrants, Kevin Hu, Sean Tran"
+    scrum_team = "GroceryStoreSim"
+    trimester = 1
+    keyword = "Games/Gaming"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def CitySimulation():
+    image = ""
+    title = "City Simulation"
+    members = "Aditya Surapaneni, Arnav Swami, Jacob Rozenkrants, Atharva Kudrimoti"
+    scrum_team = "DebugDataBandits"
+    trimester = 2
+    keyword = "Simulation"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
+    return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
+
+
+def StudenTools():
+    image = ""
+    title = "StudentTools"
+    members = "Aditya Surapaneni, Yasaswi Singamneni, Neil Sahai, Praveen Natarajan, Vihan Jayaraman"
+    scrum_team = "Booleans"
+    trimester = 3
+    keyword = "School/Education"
+    gitlink = ""
+    comlink = ""
+    runlink = "#"
+    scrumlink = "#"
+    readmelink = ""
+    description = ""
     return filterdict(image, title, members, scrum_team, trimester, keyword, gitlink, comlink, runlink, scrumlink, readmelink, description)
 
 
